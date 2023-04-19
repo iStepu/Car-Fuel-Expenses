864827f36bb2d695853c2032856b7b7a4f2ac389
Stephan <stephan.gu@edu.turkuamk.fi>
2023-04-19 22:56:48 +0300

    Updated files
    took out unnecessary files and started to make objects in Cars.js

---
+++ my-app/src/components/Cars.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const GasCars = {
+    refuel: 'l',
+    price: '€',
+    distance: 'km',
+    register: 'AB-1'
+};
+
+const ElectricCars = {
+    refuel: 'kWh',
+    price: '€',
+    distance: 'km',
+    register: 'ABC-123'
+};
+
+
