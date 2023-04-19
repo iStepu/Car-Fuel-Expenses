2f8852ca723fa45e37ddad34cadf73a2d9e91ed9
Teemu <teemu.kivinen@edu.turkuamk.fi>
2023-04-20 01:10:00 +0300

    lisätty RefuelingHistory joka listaa tankkaushistorian, sekä muutettu Cars.js muuttujien järjestystä selkeämmäksi

--- my-app/src/components/Cars.js
+++ my-app/src/components/Cars.js
@@ -1,17 +1,17 @@
 import React, {useState} from 'react';
 
 const GasCars = {
+    register: 'AB-1'
     refuel: 'l',
     price: '€',
     distance: 'km',
-    register: 'AB-1'
 };
 
 const ElectricCars = {
+    register: 'ABC-123'
     refuel: 'kWh',
     price: '€',
     distance: 'km',
-    register: 'ABC-123'
 };
 
 
