9de2cb718bf25165506c25bfab8a45d5d7cfe7a1
Jimi <jimi.hietakangas@edu.turkuamk.fi>
2023-04-22 20:54:20 +0300

    update header, added transaction list
    i followed the instructions in the video linked in the assignment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Header }from './components/Header';
+import { TransactionList }from './components/TransactionList';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <TransactionList />
     </div>
   );
 }
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker
+            Car Fuel Expense Tracker
         </h2>
     )
 }
