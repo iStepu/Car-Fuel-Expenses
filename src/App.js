2fc746ef8561fa053818b1dfb0a109af0a00bba2
Stephan <stephan.gu@edu.turkuamk.fi>
2023-04-24 23:53:40 +0300

    Bug fixes and making app launching work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Header }from './components/Header';
-import { TransactionList }from './components/TransactionList';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <TransactionList />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
